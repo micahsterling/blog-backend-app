b588fc1b461bd03a644d7944af5e52da1d70de14
Micah Sterling <msterling89@gmail.com>
2021-04-14 20:46:14 -0500

    add test create action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -21,5 +21,35 @@ RSpec.describe "Posts", type: :request do
       expect(posts.length).to eq(1)
     end
   end
- 
+  describe "Post /posts" do 
+    it "should create a new post in the db" do 
+      # make a web request to rails that makes a new post
+      user = User.create!(
+        email: "frank@email.com",
+        password: "password" 
+      )
+
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+
+      post "/api/posts", params: {
+        title: "the title",
+        body: "the body",
+        image: "the image",
+      },
+
+      headers: {"Authorization" => "Bearer #{jwt}"}
+      # parse the response
+      recipe = JSON.parse(response.body)
+      # check that the correct data is in the response  
+      expect(response).to have_http_status(200)
+      expect(post["title"]).to eq("the title")
+    end
+  end
 end
